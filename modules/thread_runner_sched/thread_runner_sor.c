e119458a82261fb9b8718aca6ade3409d334fed5
EduardoSpode <80767383+EduardoSpode@users.noreply.github.com>
2023-06-16 22:24:54 +0000

    add alguma coisa

--- modules/thread_runner_sched/thread_runner_sor.c
+++ modules/thread_runner_sched/thread_runner_sor.c
@@ -4,37 +4,20 @@
 #include <unistd.h>
 #include <string.h>
 #include <linux/sched.h>
-#include <semaphore.h>
 
-// Buffer global
-char* buffer;
-int ponter = 0;
-
-int buffer_size;
+char *buffer = NULL;
+int  pointer =0;
 
 //Semaforo
 pthread_mutex_t semaforo;
-
-// Barreira
-pthread_barrier_t semaforo_wait;
 
 void *run(void *data)
 {
 	int id = (int) data;
 	char letter = 0x41 + id;
-	pthread_barrier_wait(&semaforo_wait);
-	while(1)
-	{	
-		pthread_mutex_lock(&semaforo);
-		if(ponter >= buffer_size){
-			pthread_mutex_unlock(&semaforo);
-			return 0;
-		}
-		buffer[ponter] = letter;
-		ponter++;
-		pthread_mutex_unlock(&semaforo);
-	}
-
+	printf("Inicia thread %d %c \n", id, letter);
+	buffer[pointer] = letter;
+	pointer++;
 	return 0;
 }
 
@@ -85,7 +68,6 @@ int setpriority(pthread_t *thr, int newpolicy, int newpriority)
 	print_sched(policy);
 
 	param.sched_priority = newpriority;
-	
 	ret = pthread_setschedparam(*thr, newpolicy, &param);
 	if (ret != 0)
 		perror("perror(): ");
@@ -101,75 +83,59 @@ int main(int argc, char **argv)
 {
 
 	pthread_mutex_init(&semaforo, NULL);
-	pthread_barrier_init(&semaforo_wait, NULL, atoi(argv[1]));
 
 	int N_THREADS = 0;
+	int BUFFER_SIZE = 0;
+	int POLICY_TYPE = 0;
 	int POLICY_PRIORITY = 0;
 
-	if (argc < 4){
-		printf("usage: %s <número_de_threads> <tamanho_do_buffer_global_em_kilobytes> <politica> <prioridade>\n\n", argv[0]);
+	if (argc < 5) {
+		printf("usage: ./%s <numero_de_threads> <tamanho_do_buffer_global_em_kilobytes> <politica> <prioridade>\n\n", argv[0]);
 		return 0;
 	}
 
 	N_THREADS = atoi(argv[1]);
-	buffer_size = atoi(argv[2]) * 1024;
+	BUFFER_SIZE = atoi(argv[2]) * 1024;
+	POLICY_TYPE = atoi(argv[3]);
 	POLICY_PRIORITY = atoi(argv[4]);
 
-	int sched;
+	pthread_t thr[N_THREADS];
+	buffer = (char*) malloc(sizeof(char) * BUFFER_SIZE) ;
+
+	memset(buffer, 0, sizeof(char) * BUFFER_SIZE);
+
+
+	printf("Inicio: %d %d %d %d\n", N_THREADS, BUFFER_SIZE, POLICY_TYPE, POLICY_PRIORITY);
+
+	int prioridade = atoi(argv[4]);
+
+	int sched = 0;
+
 	if(strcmp(argv[3],"SCHED_FIFO")==0) sched = SCHED_FIFO;
 	else if(strcmp(argv[3],"SCHED_RR")==0) sched = SCHED_RR;
-	else if(strcmp(argv[3],"SCHED_IDLE")==0) sched = SCHED_IDLE;
+
+	else if(strcmp(argv[3],"SCHED_DEADLINE")==0) sched = SCHED_DEADLINE;
 	else if(strcmp(argv[3],"SCHED_OTHER")==0) sched = SCHED_OTHER;
 	else if(strcmp(argv[3],"SCHED_BATCH")==0) sched = SCHED_BATCH;
-	else if(strcmp(argv[3],"SCHED_DEADLINE")==0) sched = SCHED_DEADLINE;
-	
+	else if(strcmp(argv[3],"SCHED_IDLE")==0) sched = SCHED_IDLE;
 
-	pthread_t thr[N_THREADS];
-
-	// buffer init 
-	buffer = (char *) malloc(buffer_size);
-	memset(buffer, 0, buffer_size);
-
-	for(int i = 0; i < N_THREADS; i++){
-		pthread_create(&thr[i], NULL, run, (void *) i);
-		setpriority(&thr[i], sched, POLICY_PRIORITY);
+	for(int i = 0; i < N_THREADS; i++) {
+			pthread_create(&thr[i], NULL, run, (void*)i);
+			setpriority(&thr, sched, prioridade);
 	}
 
-	for(int i = 0; i < N_THREADS; i++){
+	for(int i = 0; i < N_THREADS; i++) {
 			pthread_join(thr[i], NULL);
 	}
 
-	pthread_barrier_destroy(&semaforo_wait);
+	// for(int i = 0; i < BUFFER_SIZE; i++) {
+	// 			printf("Buffer %c \n", buffer[i] );
+
+	// }
+
+	printf("Fim\n");
+
 	pthread_mutex_destroy(&semaforo);
-
-	//Mostra a sequencia
-	for(int i = 0; i < buffer_size; i++){
-		printf("%c", buffer[i]);
-	}
-
-
-	int* thread_rep_counter = (int*) malloc(N_THREADS * sizeof(int));
-	memset(thread_rep_counter,0,N_THREADS);
-
-	//Mostra a quantidade sem repeticoes
-	for(int i = 0; i < buffer_size; i++){
-		int index = (int)buffer[i]-0x41;
-		if(i==0){
-			printf("%c", buffer[i]);
-			thread_rep_counter[index] = thread_rep_counter[index] + 1;
-		} 
-
-		if(i > 0 && buffer[i] != buffer[i-1]){
-			printf("%c", buffer[i]);
-			thread_rep_counter[index] = thread_rep_counter[index] + 1;
-		}
-	}
-	
-	printf("\n\n");
-	for(int i = 0; i < N_THREADS; i++){
-		printf("%c = %d\n", i+0x41 ,thread_rep_counter[i]);
-	}
-
+	free(buffer);
 	return 0;
 }
-
